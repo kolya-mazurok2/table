3643e0b5513fb6282b2ec40ff98a7090e6959776
kolya-mazurok2 <38666510+kolya-mazurok2@users.noreply.github.com>
2022-07-29 15:44:34 +0300

    Revert "Resolved #4 (#6)" (#7)
    This reverts commit fdb99f5f946109c0ce72a10b24922a08013bef9d.

--- src/services/http/fake-authors/index.ts
+++ src/services/http/fake-authors/index.ts
@@ -1,8 +1,7 @@
 import { authors } from '../../../db';
-import { Author } from '../../../types/author';
 import { DEFAULT_HTTP_RESPONSE } from '../constants';
 import { HTTP_RESPONSE } from '../types';
 
-export const getAll = (): Promise<HTTP_RESPONSE<Author[]>> => {
+export const getAll = (): Promise<HTTP_RESPONSE> => {
   return Promise.resolve({ ...DEFAULT_HTTP_RESPONSE, data: authors });
 };
