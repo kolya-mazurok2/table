ef20913293486b63c08dceeb6258ea62d8a85455
kolya-mazurok2 <38666510+kolya-mazurok2@users.noreply.github.com>
2022-07-29 15:49:44 +0300

    4 fix http response (#8)

--- src/services/http/fake-authors/index.ts
+++ src/services/http/fake-authors/index.ts
@@ -1,7 +1,8 @@
 import { authors } from '../../../db';
+import { Author } from '../../../types/author';
 import { DEFAULT_HTTP_RESPONSE } from '../constants';
 import { HTTP_RESPONSE } from '../types';
 
-export const getAll = (): Promise<HTTP_RESPONSE> => {
+export const getAll = (): Promise<HTTP_RESPONSE<Author[]>> => {
   return Promise.resolve({ ...DEFAULT_HTTP_RESPONSE, data: authors });
 };
